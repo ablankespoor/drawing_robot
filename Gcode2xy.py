e232f04c416b84eabaa36477fb12ae4aed92fe00
ablankespoor <ablank@gmail.com>
2016-03-02 19:24:38 -0800

    Finished code to load and process G-code

--- Gcode2xy.py
+++ Gcode2xy.py
@@ -10,7 +10,8 @@ import numpy as np
 
 # load the Gcode file
 file_path = 'Data/'
-file_name = 'PelotonLinkLogoFromUnicorn.gcode'
+file_name = 'PelotonLinkLogo.gcode'
+output_file = 'PelotonLinkLogoXY.txt'
 
 with open(file_path+file_name) as f:
     gcode = f.read().splitlines()
@@ -19,8 +20,9 @@ f.close()
 # Iterate through the gcode and fine the trajectory (lines with "G1")
 path_x = list()
 path_y = list()
+path   = list()
 
-for line in gcode[0:1320]:
+for line in gcode[0:1300]:
     # Save the lines that start with G1
     if line.find('G1') == 0:
         # Save the x-y coordinates
@@ -31,20 +33,41 @@ for line in gcode[0:1320]:
         # Exlude the "Peloton" text (couldn't crop in Inkscape...)
         x = float(line[index_x1+1:index_x2])
         y = float(line[index_y1+1:index_y2])
-        if y > -100:
+        if y > -30:
             path_x.append(x)
             path_y.append(y)
+            path.append([x,y])
 
+
+path = np.array(path)
+
+##print(path[0:5][:])
+### Move the initial point to the origin
+##for index in range(1,path.shape[0]):
+###for index in range(1,5):
+##    print(str(index) + '  ' + str(path[index,0]) + '  ' + str(path[index, 1]))
+##    path[index][0] = path[index][0] - path[0][0]
+##    path[index,1] = path[index,1] - path[0][1]
+##path[0][:] = [0, 0]
+##
+##print()
+##print(path[0:5][:])
+
+offset = path[0,:]
+path = path - offset
 
 
 # Plot the xy points
-plt.plot(path_x,path_y,'.')
+plt.plot(path[:,0],path[:,1],'.-',path[0][0],path[0][1],'o')
 plt.title('X-Y Coordinates from Gcode')
 plt.xlabel('x [mm]')
 plt.ylabel('y [mm]')
 plt.grid(True)
 plt.show()
 
+# Save the path data and export to Raspberry Pi for plotting
+np.savetxt('Data/PelotonLogoXY.csv',path,delimiter=",")
+ 
 
 
 
