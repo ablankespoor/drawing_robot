70d1e825e4ca7c7ab747ca1d05268baff5f5d17c
ablankespoor <ablankespoor@users.noreply.github.com>
2017-06-28 14:02:24 -0700

    Implement logging and new main (#11)
    * Get _main to run and generate logs
    
    * Clean up _main with functions
    
    * Ignore log files, small updates
    
    * Draw Yoda and run robot
    
    * Load file by number from a list

--- load_my_file.py
+++ load_my_file.py
@@ -2,20 +2,22 @@
 #
 # load_my_file.py
 #
-def file_namer():
+def file_namer(file_number,dot_extension):
     # Load the Gcode file
-    
+
+    file_list = ['zero','Star-Wars-Yoda', 'cat_outline_11_9_16_tsp']
     #file_name = 'PelotonLinkLogo.gcode'
-    #file_name = 'cat_outline_from_unicorn.gcode'
+    #file_name = 'cat_outline_from_unicorn'
     #file_name = 'circle_from_unicorn.gcode'
-    file_name = 'Star-Wars-Yoda.gcode'
+    file_name = 'Star-Wars-Yoda'
     #file_name = 'yoda_from_plotterize.gcode'
     #file_name = 'sitting_cat_outline.gcode'
     #file_name = 'tiger_10_4_16_with_unicorn.gcode'
+    #file_name = 'cat_outline_11_9_16_tsp'
 
-    print('loading data from: '+file_name)
+    ###print('loading data from: '+file_name+dot_extension)
 
-    return file_name
+    return file_list[file_number]+dot_extension
 
 
 def file_path_os(win_or_pi):
@@ -23,6 +25,7 @@ def file_path_os(win_or_pi):
         file_path = 'DrawingInputFiles/'
     if win_or_pi == 'pi':
         file_path = '/home/pi/Documents/drawing_robot/DrawingInputFiles/'
+        
 
     return file_path
 
@@ -31,3 +34,5 @@ def file_path_os(win_or_pi):
 
 if __name__ == '__main__':
     print('Running: load_my_file.py, as MAIN')
+
+    print(file_namer(1,'.gcode'))
